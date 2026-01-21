6cc384dcd18c23d98bd1d0a09a7d982966c9a523
Oguzhan Baran <oguzhanbaran@outlook.com>
2026-01-21 13:58:09 +0300

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+    site: 'https://aspiresoft.github.io',
+    base: '/dorm-finder-astro',
     integrations: [tailwind()],
 });
